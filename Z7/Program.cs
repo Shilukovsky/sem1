c296c29b7eeaa372f4222a242fba0e34c5cb9d02
Nikolay <cccp_ussr@bk.ru>
2022-07-09 10:23:00 +0300

    added Z7

--- Z7/Program.cs
+++ Z7/Program.cs
@@ -1,5 +1,5 @@
-﻿
+﻿Console.Clear();
 Console.WriteLine("введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 int last = num % 10;
-console.WriteLine(last)
+Console.WriteLine($"последнее число {last}");
