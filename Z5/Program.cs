ab827288ed12305fcc132be133fc1fc8e8bc4b9c
Nikolay <cccp_ussr@bk.ru>
2022-07-09 10:00:19 +0300

    added Z5

--- Z5/Program.cs
+++ Z5/Program.cs
@@ -1,2 +1,7 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("введите натуральное число");
+int n = int.Parse(Console.ReadLine());
+int i = -n;
+while (i <= n);
+     Console.Write($"{i}");
+     i++;
