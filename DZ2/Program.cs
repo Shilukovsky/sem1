0716b9ff7f2bc4b4ae78a59bc18b66e9e37c3fdc
Nikolay <cccp_ussr@bk.ru>
2022-07-09 11:21:42 +0300

    added DZ8

---
+++ DZ2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("введите через enter два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2)
+{
+    Console.WriteLine($"число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"число {num1} меньше числа {num2}");
+}
+else
+{
+    Console.WriteLine($"число {num1} равно числу {num2}");
+}
