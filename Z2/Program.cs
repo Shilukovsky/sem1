18d28ede605608d27733d628c51ae697b6ec6dec
Nikolay <cccp_ussr@bk.ru>
2022-07-09 09:41:48 +0300

    added Z3

--- Z2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("введите через enter два целых числа");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 == num2*num2)
-Console.WriteLine($"{num1} является квадратом {num2}");
-else Console.WriteLine($"{num1} не является квадратом {num2}");   
