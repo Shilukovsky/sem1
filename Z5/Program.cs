b7a2d789b74fa7635d45cc8f9fce8b1210cb8f52
Nikolay <cccp_ussr@bk.ru>
2022-07-09 10:11:34 +0300

    added Z5

--- Z5/Program.cs
+++ Z5/Program.cs
@@ -1,7 +1,18 @@
 ﻿Console.Clear();
 Console.WriteLine("введите натуральное число");
 int n = int.Parse(Console.ReadLine());
-int i = -n;
-while (i <= n);
+int i = 0;
+if (n<0)
+{
+     i = n;
+     n = n * -1;
+}
+else
+{
+     i = -n;
+}          
+while (i <= n)
+{     
      Console.Write($"{i}");
      i++;
+}
