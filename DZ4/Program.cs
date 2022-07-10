16386ee5826e480d5c3881ef5bfbaa77e9dbe886
Nikolay <cccp_ussr@bk.ru>
2022-07-10 07:16:39 +0300

    added DZ6

--- DZ4/Program.cs
+++ DZ4/Program.cs
@@ -3,17 +3,17 @@ Console.WriteLine("введите через enter три числа");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
-if(num1>num2);
+if(num1>num2)
     if(num1>num3)
 {
     Console.WriteLine($"{num1} самое большое число");  
 }
-else if(num2>num1);
+else if(num2>num1)
      if(num2>num3)
 {
     Console.WriteLine($"{num2} самое большое число");  
 }    
-else if(num3>num1);
+else if(num3>num1)
      if(num3>num2)
 {
     Console.WriteLine($"{num3} самое большое число");  
---
+++ DZ6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"число {num} четное ");
+}
+else
+{
+    Console.WriteLine($"число {num} нечетное ");
+}
