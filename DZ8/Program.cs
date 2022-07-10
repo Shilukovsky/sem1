485cdd9a4dcd6f99cd40d23105644600d74c3eb0
Nikolay <cccp_ussr@bk.ru>
2022-07-10 07:36:20 +0300

    added DZ8

---
+++ DZ8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= n)
+     if(i % 2 == 0)
+{     
+       Console.Write ($"{i} ");
+  i++;
+}
+    else
+    {
+     i++;
+    } 
